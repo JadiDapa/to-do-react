63385e74e771c3273e9ca9bd1889b8067732df3b
JadiDapa <daffafalthaf25@gmail.com>
2024-05-17 21:54:53 +0700

    feat : create ToDoWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import style from "../css/ToDoList.module.css";
+import ToDoWrapper from "./ToDoWrapper";
+
 export default function App() {
-  return <div>To Do App</div>;
+  return (
+    <div className={style.TodoList}>
+      <ToDoWrapper />
+    </div>
+  );
 }
